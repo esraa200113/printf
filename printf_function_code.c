316baa47c251cd48bab08c31e10294c29d8a8af5
esraa200113 <esraa200113@gmail.com>
2024-02-03 03:34:41 +0200

    printf basic code

---
+++ printf_function_code.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _printf - Prints output according to a format.
+ * @format: Format string.
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings).
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					count += write(1, (char[]){va_arg(args, int)}, 1);
+					break;
+				case 's':
+					count += write(1, va_arg(args, char*), 1);
+					break;
+				case '%':
+					count += write(1, "%", 1);
+					break;
+				default:
+					count += write(1, "%", 1);
+					break;
+			}
+		}
+		else
+		{
+			count += write(1, format, 1);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
+
